3ccdd19a8d10a61fc4337c943b451c81da2f05d5
WadimSh <wadim11@yandex.ru>
2023-11-14 12:13:16 +0300

    feat: развернута база данных

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('<p>сервер работает</p>');
